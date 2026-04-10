e0900351a8544375c005e2d0ec253c122d3e0c24
victor-Silvadev <notebookconta037@gmail.com>
2026-04-10 18:06:43 -0300

    nova funçoao do js

--- ex 026/mq005/script.js
+++ ex 026/mq005/script.js
@@ -2,8 +2,11 @@ function abrirMenu(){
 
 if (opcoes.style.display == 'block') {
     opcoes.style.display = 'none'
+    burguer.style.background = `rgb(69, 29, 115`
 } else {
     opcoes.style.display = 'block'
+    burguer.style.background = "none"
+
 }
 
 }
